3343ab6e7e84b4596d48d7deb2ca617644e299f8
abdelillah <aghomari.professionnel@gmail.com>
2022-01-23 23:35:59 +0100

    before major

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
     networks: {
       hardhat: {
+        chainId: 1337
       },
       localhost: {
         url: "http://127.0.0.1:8545"
