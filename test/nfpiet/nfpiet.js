d338145519314944676880893f41cf685b150a37
abdelillah <aghomari.professionnel@gmail.com>
2022-01-24 02:36:10 +0100

    tient

--- test/nfpiet/nfpiet.js
+++ test/nfpiet/nfpiet.js
@@ -29,12 +29,21 @@ describe("NFpieT", function() {
     let balance = await nfpiet.balanceOf(recipient);
     expect(balance).to.equal(0);
 
-    const newlyMintedToken = await nfpiet.payToMint(
+    const newlyMintedToken = await nfpiet.callStatic.payToMint(
       recipient, 
       metadata.name, 
       JSON.stringify(metadata.codels), 
       { value: ethers.utils.parseEther('0.05') }
       );
 
+    expect(newlyMintedToken).to.equal(0);
+
+    const data = '{ name: "", Descripiton: " NFpieT is a community generated token that represents a code in the esoteric language Piet.", image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaW5ZTWluIG1lZXQiIHZpZXdCb3g9IjAgMCAwIC8iPjwvc3ZnPg===="}'
+
+    let tokenUri = await nfpiet.tokenURI(newlyMintedToken);
+
+    let decodedPayload = Buffer.from(tokenUri.replace('data:application/json;base64,',''), 'base64').toString('utf-8');
+
+    expect(decodedPayload).to.equal(data);
   });
 });
